c4cfe666645e556ee9a66a48d34cff67566979c5
guita13921 <guita13921@gmail.com>
2022-02-01 14:53:39 +0700

    lab21_2.cpp complete

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,35 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+	Rect area;
+	if((R1 ->x +R1 ->w) - R2 ->x > 0){
+		area.x = R2 ->x;
+		area.w = (R1 ->x+R1 ->w) - R2 ->x;
+		if((R1 ->x+R1 ->w) > (R2 ->x+R2 ->w)){
+			area.x = R2 ->x;
+			area.w = R2 ->w;
+		}
+	}else if((R1 ->x-R1 ->w) - R2 ->x <= 0){
+		area.x = 0;
+		area.w = 0;
+	}
 
-
-
+	if(R2 ->y -(R1 ->x-R1 ->h)>0){
+		area.y = R1 ->y;
+		area.h = R1 ->y -(R2->y-R2->h);
+		if((R1->y-R1->h)<(R2->y-R2->h) and R1->y > R2->y){
+			area.y = R2->y;
+			area.h = R2->h;
+		}
+	}else if((R1->x+R1->w) - R2->x <= 0){
+		area.y =0;
+		area.h = 0;
+	}
+	return area.w*area.h;
+}
+int main(){
+	Rect R1 = {-1,2,6.9,9.6};
+	Rect R2 = {0,0,1.2,2.5};
+	cout << overlap(&R1,&R2);
 }
